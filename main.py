22f9fd9ee62753ec52785798bb410cb4aa426ce5
kitaclysm <murraykyleb@gmail.com>
2025-12-24 14:18:39 -0800

    move ship and spawn asteroids

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
-    # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidField = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         log_state()
+        
+        # set scene
         screen.fill("black")
+
+        # allow inputs
+        updatable.update(dt)
+
+        for drawn in drawable:
+            drawn.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(f"dt: {dt}")
 
 if __name__ == "__main__":
     main()
