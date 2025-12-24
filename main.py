b34d32342caefcfd9c1e56aea8ba91965236afb7
kitaclysm <murraykyleb@gmail.com>
2025-12-24 14:27:40 -0800

    added collision

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -32,6 +33,12 @@ def main():
 
         # allow inputs
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
 
         for drawn in drawable:
             drawn.draw(screen)
